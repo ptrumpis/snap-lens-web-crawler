f4cdab0bd0b1c3dfba564ca601db690521bd5cf4
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-02-08 22:32:43 +0100

    Add script import-csv to crawl lenses from csv files

---
+++ src/scripts/importCsv.js
@@ -0,0 +1,21 @@
+import * as Utils from "./utils/functions.js";
+import process from 'process';
+
+const overwriteBolts = false;
+const overwriteExistingData = false;
+const saveIncompleteLensInfo = false;
+
+const inputFile = process.argv[2];
+if (!inputFile) {
+    console.error('No input file specified.');
+    process.exit(1);
+}
+
+try {
+    const lenses = await Utils.readCSV(inputFile);
+    if (lenses && lenses[0]?.lens_id && lenses[0]?.uuid) {
+        await Utils.crawlLenses(lenses, { overwriteExistingData, saveIncompleteLensInfo, overwriteBolts });
+    }
+} catch (e) {
+    console.error(e);
+}
