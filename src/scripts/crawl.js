96517970e77cc6dcb7dcbfae0b4c04d9ecae8701
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-04-24 21:34:34 +0200

    Add command line arguments to crawl script

--- src/scripts/crawl.js
+++ src/scripts/crawl.js
@@ -1,6 +1,15 @@
 import SnapLensWebCrawler from "../lib/crawler.js";
 import * as Utils from "./utils/functions.js";
 
+const args = new Set(process.argv.slice(2));
+
+const options = {
+    retryBrokenDownloads: args.has('--retry-broken-downloads'),
+    overwriteExistingBolts: args.has('--overwrite-existing-bolts') || args.has('--overwrite-existing'),
+    overwriteExistingData: args.has('--overwrite-existing-data') || args.has('--overwrite-existing'),
+    saveIncompleteLensInfo: args.has('--save-incomplete-lens-info'),
+};
+
 const crawler = new SnapLensWebCrawler({ maxRequestRetries: 2, gcInterval: false });
 const resolvedLensCache = new Set();
 
@@ -12,7 +21,7 @@ for (const category in crawler.TOP_CATEGORIES) {
         if (topLenses && topLenses.length) {
             console.log(`[Resolving] ${topLenses.length} Lenses from Category: ${category.toUpperCase()}`);
 
-            await Utils.crawlLenses(topLenses, { crawler, resolvedLensCache, queryRelayServer: false });
+            await Utils.crawlLenses(topLenses, { crawler, resolvedLensCache, queryRelayServer: false, ...options});
 
             console.log(`[Finished] ${topLenses.length} Lenses from Category: ${category.toUpperCase()}`);
             console.log(`-----`);
